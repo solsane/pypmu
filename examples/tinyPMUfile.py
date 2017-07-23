16f01d858a2fc651c896ad25fb914cab7fd3e5c8
Aiden Rutter <arutter@utk.edu>
2017-07-22 22:08:58 -0400

    Added new test method, fixed index search

---
+++ examples/tinyPMUfile.py
@@ -0,0 +1,25 @@
+from synchrophasor.pmu import Pmu
+
+
+"""
+tinyPMU will listen on ip:port for incoming connections.
+When tinyPMU receives command to start sending
+measurements - fixed (sample) measurement will
+be sent.
+"""
+
+
+if __name__ == "__main__":
+
+    pmu = Pmu(ip="127.0.0.1", port=1410)
+    pmu.logger.setLevel("DEBUG")
+
+    pmu.set_configuration()  # This will load default PMU configuration specified in IEEE C37.118.2 - Annex D (Table D.2)
+    pmu.set_header()  # This will load default header message "Hello I'm tinyPMU!"
+
+    pmu.run()  # PMU starts listening for incoming connections
+
+    while True:
+        if pmu.clients:  # Check if there is any connected PDCs
+            pmu.send_data_file("ieee14_vsc_wtg_out.lst", "ieee14_vsc_wtg_out.dat")  # Sending sample data frame specified in IEEE C37.118.2 - Annex D (Table D.1)
+    pmu.join()
